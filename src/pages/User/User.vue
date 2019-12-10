7066edd943714c3d7c211dede9af45b295608835
944263044@qq.com <f9442663044>
2019-12-10 14:49:23 +0800

    '登录注册的完善'

---
+++ src/pages/User/User.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="login_page">
+    <section class="form" >
+      <img class="logo" src="./images/login.png" />
+      <router-view/>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: 'User'
+}
+</script>
+<style scopde lang="stylus" rel="stylusheet/stylus">
+.login_page
+  width 100%
+  height 100%
+  background-color #F5F5F5
+  display flex
+  justify-content center
+  flex-wrap wrap
+  .form
+    width 854px
+    height 720px
+    background-color #fff
+    display flex
+    align-items center
+    flex-direction column
+    .logo
+      padding-top 50px
+</style>
+
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,9 @@ import Home from '../pages/Home/Home.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import ShoppingCart from '../pages/ShoppingCart/ShoppingCart.vue'
 import Order from '../pages/Order/Order.vue'
+import User from '../pages/User/User.vue'
+import Login from '../pages/User/Login/Login.vue'
+import Register from '../pages/User/Register/Register.vue'
 
 // 暴露
 export default [
@@ -30,5 +33,23 @@ export default [
   {
     path: '/',
     redirect: '/home'
+  },
+  {
+    path: '/user',
+    component: User,
+    children: [
+      {
+        path:'/user/login',
+        component: Login
+      },
+      {
+        path: '/user/register',
+        component: Register
+      },
+      {
+        path: '/user',
+        redirect: '/user/login'
+      }
+    ]
   }
 ]
