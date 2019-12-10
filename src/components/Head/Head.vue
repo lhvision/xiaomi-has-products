9f0c03dc1cf54983bfc83ac8f01f7226355a80be
Lhvision <lhvision@outlook.com>
2019-12-10 17:11:24 +0800

    小修复

--- src/components/Head/Head.vue
+++ src/components/Head/Head.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
     <div class="m-no-login">
-      <router-link class="go" to="/login">登录</router-link>
-      <router-link to="/">注册</router-link>
+      <router-link class="go" to="/user/login">登录</router-link>
+      <router-link to="/user/register">注册</router-link>
     </div>
   </div>
 </template>
@@ -12,15 +12,4 @@ export default {
 }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-.container
-  width 100%
-  height 50px
-  background-color #333
-  display flex
-  justify-content flex-end
-  align-items: center
-  .m-no-login
-    margin-right 50px
-    .go
-      margin-right 10px
 </style>
