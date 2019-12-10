8544dc8ece9afaf41df572c7b24cb04223891313
lhvision <35862649+lhvision@users.noreply.github.com>
2019-12-10 15:29:05 +0800

    Revert "Mq"

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,10 +3,6 @@ import Details from '../pages/Details/Details.vue'
 import Home from '../pages/Home/Home.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import ShoppingCart from '../pages/ShoppingCart/ShoppingCart.vue'
-import Order from '../pages/Order/Order.vue'
-import Flosh from '../pages/Profile/Flosh.vue'
-import Collection from '../pages/Profile/Collection.vue'
-import Geography from '../pages/Profile/Geography.vue'
 import User from '../pages/User/User.vue'
 import Login from '../pages/User/Login/Login.vue'
 import Register from '../pages/User/Register/Register.vue'
@@ -24,18 +20,6 @@ export default [
     path: '/profile',
     component: Profile
   },
-  {
-  path: '/flosh',
-  component: Flosh
-  },
-  {
-    path: '/collection',
-    component: Collection
-    },
-    {
-      path: '/geography',
-      component: Geography
-      },
   {
     path: '/shoppingCart',
     component: ShoppingCart
